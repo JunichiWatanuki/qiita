4eb7745fb2072f165952c29aac970cfb7f5b2411
Junichi Watanuki <junichi.watanuki@gmail.com>
2017-10-29 12:56:42 +0900

    update17

---
+++ deply/deploy.js
@@ -0,0 +1,24 @@
+global.Promise = require('bluebird');
+require('isomorphic-fetch');
+
+const fs = require( 'fs' );
+const Qiita = require( 'qiita-js' );
+
+if ( "false" !== process.env.TRAVIS_PULL_REQUEST ) {
+  console.log( 'Thanks for contribution. :)' );
+  process.exit();
+}
+
+// set your token
+Qiita.setToken( process.env.QIITA_TOKEN );
+Qiita.setEndpoint( 'https://qiita.com' );
+
+fs.readFile( 'README.md', 'utf8', ( err, readme ) => {
+  Qiita.Resources.Item.update_item( process.env.QIITA_POST_ID, {
+    title: process.env.QIITA_POST_TITLE,
+    body: readme,
+    private: false,
+  } ).then( function( res ) {
+    console.log( res );
+  } )
+} );
